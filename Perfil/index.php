7f7fc16855457ebf7753fa0cd421d12799ac2075
Déborah Rodrigues <33472633+deborahNRodrigues@users.noreply.github.com>
2023-06-29 13:20:00 -0400

    Perfil Profissional
    atualização do perfil profissional para divulgação de travalhos e portfolio

---
+++ Perfil/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+  <html lang="pt-br">
+    <head>
+    	<title>Perfil Apresentação</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <!-- Navbar goes here -->
+        <nav>
+    <div class="nav-wrapper cyan darken-1">
+      <a href="#" class="brand-logo"></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Home</a></li>
+        <li><a href="badges.html">About</a></li>
+        <li><a href="collapsible.html">Portfólio</a></li>
+      </ul>
+    </div>
+  </nav>
+
+    <!-- Page Layout here -->
+    <div class="row">
+
+      <div class="col s3">
+        <!-- Grey navigation panel -->
+        <div class="card large cyan darken-1">
+           <div class="col s12 m8 offset-m2 l6 offset-l3">
+        
+                <img src="img/minha-foto.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s12">
+              <span class="white-text">
+                  Name: Déborah do Nascimento Rodrigues.<br>
+                  Graduated:Technology in Systems Analysis and Development.
+                  Knowledge in the following programming languages: Java,PHP,JavaScript.
+                  Hypertext markup languages:HTML5.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+            
+    <!-- Card Content -->
+  </div>
+            
+      </div>
+
+      <div class="col s9">
+        <!-- Teal page content  -->
+      </div>
+
+    </div>
+
+    	
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
+        
