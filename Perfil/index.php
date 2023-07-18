c9daaea79f69b89df67f9d3aee20c5345986e21e
Déborah Rodrigues <33472633+deborahNRodrigues@users.noreply.github.com>
2023-07-18 17:32:06 -0400

    Atualização de Arquivos
    Atualização de arquivos de home, inclusão de divisões de section e icones

--- Perfil/index.php
+++ Perfil/index.php
@@ -1,67 +1,95 @@
 <!DOCTYPE html>
 
 <!DOCTYPE html>
-  <html lang="pt-br">
+<html lang="pt-br">
     <head>
-    	<title>Perfil Apresentação</title>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <title>Perfil Apresentação</title>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        
+        <!--Import reset.css-->
+        <link type="text/css" rel="stylesheet" href="css/reset.css"/>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Import estilo.css-->
+        <link type="text/css" rel="stylesheet" href="css/estilo.css"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
     <body>
         <!-- Navbar goes here -->
-        <nav>
-    <div class="nav-wrapper cyan darken-1">
-      <a href="#" class="brand-logo"></a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Home</a></li>
-        <li><a href="badges.html">About</a></li>
-        <li><a href="collapsible.html">Portfólio</a></li>
-      </ul>
-    </div>
-  </nav>
-
-    <!-- Page Layout here -->
-    <div class="row">
-
-      <div class="col s3">
-        <!-- Grey navigation panel -->
-        <div class="card large cyan darken-1">
-           <div class="col s12 m8 offset-m2 l6 offset-l3">
+        <section class="menu">
+            <nav class="menuEscolha">
+                <div class="nav-wrapper cyan darken-1">
+                    <a href="#" class="brand-logo"></a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="sass.html">Home</a></li>
+                        <li><a href="badges.html">About</a></li>
+                        <li><a href="collapsible.html">Portfólio</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </section>
+        <!-- Page Layout here -->
         
-                <img src="img/minha-foto.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+    <section class="perfilLateral">
+        <div class="row">
+
+            <div class="col s4">
+                <!-- Grey navigation panel -->
+                <div class="card large cyan darken-1">
+                    <div class="col s12 m8 offset-m2 l6 offset-l3">
+
+                        <img src="img/minha-foto.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    </div>
+                    <div class="col s12">
+
+                       
+                            <header>
+                                <h3 class="titulo">Apresentação</h3>
+                                <span class="white-text" id="apresentacaoTexto">
+                                    Name: Déborah do Nascimento Rodrigues.
+                                    Graduated:Technology in Systems Analysis and Development.
+                                </span>
+                         </header>
+                    </div>
+                </div>
             </div>
-            <div class="col s12">
-              <span class="white-text">
-                  Name: Déborah do Nascimento Rodrigues.<br>
-                  Graduated:Technology in Systems Analysis and Development.
-                  Knowledge in the following programming languages: Java,PHP,JavaScript.
-                  Hypertext markup languages:HTML5.
-              </span>
-            </div>
-          </div>
         </div>
-      </div>
-            
-    <!-- Card Content -->
-  </div>
-            
-      </div>
-
-      <div class="col s9">
-        <!-- Teal page content  -->
-      </div>
-
-    </div>
-
-    	
-      <!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
+         
+           </section>
         
+     
+        <div class="row">
+            <section class="icone-home">
+            <div class="col s6 offset-s4">
+                <img src="img/servico.png" width="width" height="height" alt="alt"/>
+                <header>
+                    <section>
+                        <div class="card small ">
+                            <h5 class="textohome">Home</h5>   
+                    
+                        </div>
+                       
+                    </section>
+                  
+                </header>
+            </div>
+            </section>
+        </div>
+        <!-- Card Content -->
+        
+        
+
+
+
+
+
+<!--JavaScript at end of body for optimized loading-->
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
